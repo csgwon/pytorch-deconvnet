75015f6f37d21de5a3de6d13dbed98a90e9b0c86
Chul Gwon <gwoncs@new-host.home>
2017-08-30 10:47:00 -0400

    added biases into transpose conv

--- models/vgg16_deconv.py
+++ models/vgg16_deconv.py
@@ -93,6 +93,7 @@ class VGG16_deconv(torch.nn.Module):
     def __init__(self):
         super(VGG16_deconv, self).__init__()
         self.conv2DeconvIdx = {0:17, 2:16, 5:14, 7:13, 10:11, 12:10, 14:9, 17:7, 19:6, 21:5, 24:3, 26:2, 28:1}
+        self.conv2DeconvBiasIdx = {0:16, 2:14, 5:13, 7:11, 10:10, 12:9, 14:7, 17:6, 19:5, 21:3, 24:2, 26:1, 28:0}
         self.unpool2PoolIdx = {15:4, 12:9, 8:16, 4:23, 0:30}
         
         self.deconv_features = torch.nn.Sequential(
@@ -142,9 +143,10 @@ class VGG16_deconv(torch.nn.Module):
         # initializing weights using ImageNet-trained model from PyTorch
         for i, layer in enumerate(vgg16_pretrained.features):
             if isinstance(layer, torch.nn.Conv2d):
-                idx = self.conv2DeconvIdx[i]
-                self.deconv_features[idx].weight.data = layer.weight.data
-                #self.deconv_features[idx].bias.data = layer.bias.data
+                self.deconv_features[self.conv2DeconvIdx[i]].weight.data = layer.weight.data
+                biasIdx = self.conv2DeconvBiasIdx[i]
+                if biasIdx > 0:
+                    self.deconv_features[biasIdx].bias.data = layer.bias.data
                 
 
     def forward(self, x, layer_number, map_number, pool_indices):
@@ -153,11 +155,8 @@ class VGG16_deconv(torch.nn.Module):
             raise ValueError('Layer '+str(layer_number)+' is not of type Conv2d')
         # set weight and bias
         self.deconv_first_layers[start_idx].weight.data = self.deconv_features[start_idx].weight[map_number].data[None, :, :, :]
-        #bias = float(self.deconv_features[start_idx].bias[map_number].data.numpy()[0])
-        bias=0.
-        
+        self.deconv_first_layers[start_idx].bias.data = self.deconv_features[start_idx].bias.data        
         # first layer will be single channeled, since we're picking a particular filter
-        self.deconv_first_layers[start_idx].bias.data = torch.FloatTensor(np.array([bias for _ in range(self.deconv_first_layers[start_idx].bias.size()[0])]))
         output = self.deconv_first_layers[start_idx](x)
 
         # transpose conv through the rest of the network
